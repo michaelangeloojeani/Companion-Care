33c737c8ca14a52781300dc62dce0df3ebbd005f
michaelangeloojeani <128701443+michaelangeloojeani@users.noreply.github.com>
2025-04-26 22:01:10 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'companion-care',
+  webDir: 'www'
+};
+
+export default config;
